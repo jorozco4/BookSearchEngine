9a6accea6e27ff64a267aae96dd58b3e9951ae73
Jesse Orozco <orozcojesse0@gmail.com>
2021-07-10 17:49:56 -0400

    applied apollo server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
 const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
