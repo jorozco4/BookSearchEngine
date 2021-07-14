bcd370d1b7d456fb8bef1de0478a704e68f1b1c7
Jesse Orozco <orozcojesse0@gmail.com>
2021-07-14 19:20:25 -0400

    first push to heroku

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const db = require("./config/connection");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
-// const routes = require("./routes");
+const routes = require("./routes");
 const { authMiddleware } = require("./utils/auth");
 
 const app = express();
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-// app.use(routes);
+app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
