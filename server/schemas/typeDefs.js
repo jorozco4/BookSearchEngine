da0cfbf9df0f7ea85045b736f8639b65cf8f478c
Jesse Orozco <orozcojesse0@gmail.com>
2021-07-15 22:02:18 -0400

    deploy

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -46,7 +46,7 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    saveBook(input: bookInput): User
+    saveBook(input: BookInput): User
     removeBook(bookId: String): User
   }
 `;
